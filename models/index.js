e6bd0f8734afeb8a4cdda3dbccd1325cc97d3766
scicluna <88948700+scicluna@users.noreply.github.com>
2023-03-02 13:53:24 -0500

    Model framework for sequel sketched out

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const Category = require("./Category")
+const Post = require("./Post")
+const User = require("./User")
+
+Post.belongsTo(Category)
+
+Post.belongsTo(User)
+
+User.hasMany(Post)
+
+Category.hasMany(Post)
+
+
+
+module.exports = {Category,Post,User,};
+  
