940496d98a1761e4136f8c38332316cabd4f9084
scicluna <88948700+scicluna@users.noreply.github.com>
2023-03-03 15:19:08 -0500

    Initial handlebars setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars');
+const path = require("path")
 
 const sequelize = require("./connection/connection")
 const app = express()
@@ -9,11 +10,28 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.get('/', (req, res) => {
+    res.render('forum')
+})
+
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT}`))
 })
+
+//TODOs
+/////////////////////////////////////////
+//SET UP BASIC ROUTES
+//CREATE MAIN HTML TEMPLATE
+//CREATE FORUM SUBTEMPLATE
+//CREATE POST PARTIAL
+//STYLE EVERYTHING TO DISPLAY THE MESSAGES
+//SORT THE MESSAGES
+//BY USER AND BY CATEGORY
+//BE ABLE TO "LOG IN" AKA CREATE A NEW USER FOR THEMESELVES
+//BE ABLE TO MAKE POSTS
+//COMMENT ON POSTS
